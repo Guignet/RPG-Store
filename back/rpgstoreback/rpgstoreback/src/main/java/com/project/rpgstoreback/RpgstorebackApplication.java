03510be565031b8835b31af0aa041f9fabd84b32
Guignet <guillaume.peugnet.2001@gmail.com>
2022-04-21 11:20:04 +0200

    init back

---
+++ back/rpgstoreback/rpgstoreback/src/main/java/com/project/rpgstoreback/RpgstorebackApplication.java
@@ -0,0 +1,13 @@
+package com.project.rpgstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgstorebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgstorebackApplication.class, args);
+	}
+
+}
---
+++ back/rpgstoreback/rpgstoreback/src/test/java/com/project/rpgstoreback/RpgstorebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rpgstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgstorebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
