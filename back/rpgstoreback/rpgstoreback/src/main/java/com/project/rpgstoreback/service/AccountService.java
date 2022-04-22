d438db2342cee28fdc080d20fc4a3ade9bb03eac
Guignet <guillaume.peugnet.2001@gmail.com>
2022-04-22 15:25:01 +0200

    System de login en place error 403 forbidden sur le rest

--- back/rpgstoreback/rpgstoreback/src/main/java/com/project/rpgstoreback/service/AccountService.java
+++ back/rpgstoreback/rpgstoreback/src/main/java/com/project/rpgstoreback/service/AccountService.java
@@ -1,6 +1,8 @@
 package com.project.rpgstoreback.service;
 
 import com.project.rpgstoreback.models.Account;
+import com.project.rpgstoreback.models.Role;
+import com.project.rpgstoreback.models.RoleEnum;
 import com.project.rpgstoreback.repository.RoleRepository;
 import com.project.rpgstoreback.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,31 +11,36 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
-public class AccountService implements UserDetailsService {
+public class AccountService  {
 
     @Autowired
     private AccountRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Account account = userRepository
-                .findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("username " + username + " not found"));
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        account.getRoleList()
-                .forEach(r -> grantedAuthorities.add(new SimpleGrantedAuthority(r.getName().name())));
-        return new org.springframework.security.core.userdetails.User(
-                account.getUsername(),
-                account.getPassword(),
-                grantedAuthorities
-        );
+    public boolean checkUsernameExist(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Transactional
+    public Account signup(String username, String password,String firstname,String lastname,String email) {
+        Role roleUser = roleRepository.findByName(RoleEnum.USER);
+        List<Role> roleList = Arrays.asList(roleUser);
+
+        Account u = new Account(firstname,lastname,username, passwordEncoder.encode(password),email, LocalDate.now(),false, roleList);
+        return userRepository.save(u);
     }
 }
